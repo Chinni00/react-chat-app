e00b689113a9bc04a4a2043fbd781b3e23a97add
chinni00 <chinni0437@gmail>
2023-12-11 16:20:32 +0530

    removed initialization firebase

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 //import firebase from 'firebase/app'; //older version
 import firebase from 'firebase/compat/app'; //v9
 
@@ -14,17 +10,6 @@ import 'firebase/compat/auth'; //v9
 
 
 
-firebase.initializeApp({
-  // your config
-  apiKey: "AIzaSyC8v8OZBxl6PqJ5Jn9v9OIe9lrVWIlGqGc",
-  authDomain: "chat-app-5b86e.firebaseapp.com",
-  projectId: "chat-app-5b86e",
-  databaseURL:"https:/chat-app-5b86e.firebaseio.com",
-  storageBucket: "chat-app-5b86e.appspot.com",
-  messagingSenderId: "985385750666",
-  appId: "1:985385750666:web:5f06f02cb14da83f98dd73",
-  measurementId: "G-43N106JQWG"
-})
 
 const auth = firebase.auth();
 
