a9c41b939709d46bda56298eea46ecf039b37d12
chinni00 <chinni0437@gmail>
2023-12-11 16:18:05 +0530

    removed initialization firebase

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -22,17 +22,7 @@ import 'firebase/analytics';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 
-firebase.initializeApp({
-  // your config
-  apiKey: "AIzaSyC8v8OZBxl6PqJ5Jn9v9OIe9lrVWIlGqGc",
-  authDomain: "chat-app-5b86e.firebaseapp.com",
-  projectId: "chat-app-5b86e",
-  databaseURL:"https:/chat-app-5b86e.firebaseio.com",
-  storageBucket: "chat-app-5b86e.appspot.com",
-  messagingSenderId: "985385750666",
-  appId: "1:985385750666:web:5f06f02cb14da83f98dd73",
-  measurementId: "G-43N106JQWG"
-})
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
