722843d9e07dbd7f1a7572331acc1c2c4c9ea20c
chinni00 <chinni0437@gmail>
2023-12-11 16:07:47 +0530

    packages for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import SignIn from './components/SignIn';
+import SignOut from './components/SignOut';
+import ChatRoom from './components/ChatRoom';
+
+
+//import firebase from 'firebase/app'; //older version
+import firebase from 'firebase/compat/app'; //v9
+
+//to use auth
+//import 'firebase/auth'; //older version
+import 'firebase/compat/auth'; //v9
+
+//to use firestore
+//import 'firebase/firestore'; //Older Version
+import 'firebase/compat/firestore'; //v9
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+
+
+
+const auth = firebase.auth();
+
+
+
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>V Chat</h1>
+        <SignOut />
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
+
+
+
+
+
+
+
 
 export default App;
